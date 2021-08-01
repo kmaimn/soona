061945943cc4b6f6d2db3f4f1ac8f6c057ca0e33
Kim Mai <kmai.mn@gmail.com>
2021-08-01 10:09:04 -0500

    add types

--- minesweeper.ts
+++ minesweeper.ts
@@ -1,5 +1,15 @@
-export const annotate = (input) => {
-    let tableArr = [];
+type stringOrNumber = string | number;
+
+interface ICountMines {
+    tableArr: stringOrNumber[][],
+    rowIndex: number,
+    currentCount: number,
+    cellIndex: number,
+    rowType: 'above' | 'below'
+}
+
+export const annotate = (input: string[]): string[] => {
+    let tableArr: stringOrNumber[][] = [];
 
     if(input.length === 0 || input.some(x => x === ''))
     {
@@ -9,7 +19,7 @@ export const annotate = (input) => {
     // create new array with row counts;
     input.forEach((row) => {
         let counter = 0;
-        let rowArr = [];
+        let rowArr: stringOrNumber[] = [];
 
         for(let i = 0; i < row.length; i++)
         {
@@ -34,7 +44,9 @@ export const annotate = (input) => {
     // get new counts with rows above/below current
     if(input.length > 1)
     {
-        tableArr.forEach((row, index) => {
+            let finalArr: string[] = [];
+            
+            tableArr.forEach((row, index) => {
             row.forEach((cell, i) => {
                 if(cell !== "*")
                 {
@@ -59,8 +71,10 @@ export const annotate = (input) => {
                 }
             })
 
-            tableArr[index] = row.toLocaleString().replace(/,/g,'');
+            finalArr[index] = row.toLocaleString().replace(/,/g,'');
         })
+            
+            return finalArr;
     }
     else
     {
@@ -70,7 +84,13 @@ export const annotate = (input) => {
     return tableArr;
 };
 
-const countMines = ({tableArr, rowIndex, currentCount, cellIndex, rowType}) => {
+const countMines = ({
+    tableArr,
+    rowIndex,
+    currentCount,
+    cellIndex,
+    rowType
+}: ICountMines) => => {
     let newCount = currentCount;
     if(rowType === 'above')
     {
